cc6fe477bac40755c356df767cb8de3e93326e68
kangzhiming9527 <hb_kzm@163.com>
2019-04-04 06:54:41 +0800

    增加服务器IP验证，只接受服务器消息

---
+++ src/main/java/com/qq/weixin/filter/AccessFilter.java
@@ -0,0 +1,47 @@
+package com.qq.weixin.filter;
+
+import com.qq.weixin.util.AacquireServerIPS;
+import com.qq.weixin.util.CusAccessObjectUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * AccessFilter：
+ * 2019/4/4 5:32
+ * by kzm
+ */
+public class AccessFilter implements Filter {
+    Logger log = LoggerFactory.getLogger(AccessFilter.class);
+    private static List<String> ips;
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+//        ips = AacquireServerIPS.getServerIP();
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        if (null == ips) {
+            ips = AacquireServerIPS.getServerIP();
+        }
+        String ipAddress = CusAccessObjectUtil.getIpAddress((HttpServletRequest) servletRequest);
+        log.debug("访问IP为:" + ipAddress);
+        if (ips.contains(ipAddress)) {
+            log.debug("访问IP来自服务器");
+            filterChain.doFilter(servletRequest, servletResponse);
+        } else {
+            log.error("非法访问，访问IP不是来自服务器");
+        }
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
